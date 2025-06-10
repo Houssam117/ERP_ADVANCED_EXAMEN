a4da2499e1c8985d1389bfa96aec0ebf1222912b
Houssam <houssam@example.com>
2025-06-10 02:21:59 +0200

    change detail contrl

--- zmovements_ui5/webapp/controller/Detail.controller.js
+++ zmovements_ui5/webapp/controller/Detail.controller.js
@@ -1,91 +1,268 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
-    "sap/m/MessageToast",
-    "sap/m/MessageBox"
-], function (Controller, MessageToast, MessageBox) {
+    "sap/ui/model/json/JSONModel",
+    "sap/ui/model/Filter",
+    "sap/ui/model/FilterOperator",
+    "sap/m/MessageToast"
+], function (Controller, JSONModel, Filter, FilterOperator, MessageToast) {
     "use strict";
 
-    return Controller.extend("com.sap.mov.controller.Detail", {
+    return Controller.extend("movementsapp.controller.Movements", {
 
         onInit: function () {
-            this.oRouter = this.getOwnerComponent().getRouter();
-            this.oRouter.getRoute("detail").attachPatternMatched(this._onObjectMatched, this);
+            this.uri = "/MovementSet";
+            this._items = []; // items for create popup 
+
+            // model for the create popup
+            var oItemsModel = new JSONModel({ items: [] });
+            this.getView().setModel(oItemsModel, "itemsModel");
         },
 
-        _onObjectMatched: function (oEvent) {
-            var sMovId = oEvent.getParameter("arguments").movId;
-            var oView = this.getView();
-            var oODataModel = oView.getModel();
+        applyFilters: function () {
+            var oSelect = this.byId("typeSelect");
+            var sSelectedKey = oSelect.getSelectedKey();
 
-            var sPath = oODataModel.createKey("/MovementSet", {
-                MovId: sMovId
-            });
+            var aFilters = [];
+            if (sSelectedKey && sSelectedKey !== "all") {
+                aFilters.push(new Filter("Type", FilterOperator.EQ, sSelectedKey));
+            }
 
-            oView.bindElement({
-                path: sPath,
-                parameters: {
-                    expand: "ItemSet"
-                },
-                events: {
-                    dataRequested: function () {
-                        oView.getModel("appView").setProperty("/busy", true);
-                    },
-                    dataReceived: function () {
-                        oView.getModel("appView").setProperty("/busy", false);
-                    }
-                }
-            });
+            var oList = this.byId("entryList");
+            var oBinding = oList.getBinding("items");
+            oBinding.filter(aFilters);
+
+            console.log("Applied filter: ", sSelectedKey);
         },
 
-        onNavBack: function () {
-            var oHistory = sap.ui.core.routing.History.getInstance();
-            var sPreviousHash = oHistory.getPreviousHash();
+        onSelectionChange: function (oEvent) {
+            var oList = oEvent.getSource();
+            var oSelectedItem = oList.getSelectedItem();
 
-            if (sPreviousHash !== undefined) {
-                window.history.go(-1);
+            if (oSelectedItem) {
+                var oBindingContext = oSelectedItem.getBindingContext();
+                var sMovId = oBindingContext.getProperty("Id");
+                this._showDetail(oBindingContext, sMovId);
             } else {
-                this.oRouter.navTo("appHome", {}, true);
+                this._clearDetail();
             }
         },
 
-        onDeleteMovementPress: function () {
-            var oContext = this.getView().getBindingContext();
-            var sMovId = oContext.getProperty("MovId");
-            var oODataModel = this.getView().getModel();
-            var that = this;
+        _showDetail: function (oBindingContext, sMovId) {
+            var oDetailPage = this.byId("detailPage");
+            var oDetailContent = this.byId("detailContent");
+            var oNoDataText = this.byId("noDataText");
 
-            MessageBox.confirm(
-                this.getResourceBundle().getText("deleteConfirmation", [sMovId]), {
-                    actions: [MessageBox.Action.DELETE, MessageBox.Action.CANCEL],
-                    emphasizedAction: MessageBox.Action.DELETE,
-                    onClose: function (sAction) {
-                        if (sAction === MessageBox.Action.DELETE) {
-                            that.getView().getModel("appView").setProperty("/busy", true);
+            oDetailContent.setVisible(true);
+            oNoDataText.setVisible(false);
 
-                            var sPath = oODataModel.createKey("/MovementSet", {
-                                MovId: sMovId
-                            });
+            oDetailPage.bindElement(oBindingContext.getPath());
+            this.byId("SplitApp").toDetail(oDetailPage);
 
-                            oODataModel.remove(sPath, {
-                                success: function () {
-                                    MessageToast.show(that.getResourceBundle().getText("movementDeletedSuccess", [sMovId]));
-                                    that.getView().getModel("appView").setProperty("/busy", false);
-                                    that.oRouter.navTo("appHome", {}, true);
-                                }.bind(this),
-                                error: function (oError) {
-                                    MessageToast.show(that.getResourceBundle().getText("movementDeletionError", [sMovId]));
-                                    console.error("Error deleting movement:", oError);
-                                    that.getView().getModel("appView").setProperty("/busy", false);
-                                }.bind(this)
-                            });
-                        }
-                    }
-                }
-            );
+            // Apply filter to the items list based on selected MovId
+            var oItemsList = this.byId("itemsListDetail");
+            var aFilters = [new Filter("MovId", FilterOperator.EQ, sMovId)];
+            var oBinding = oItemsList.getBinding("items");
+            oBinding.filter(aFilters);
         },
 
-        getResourceBundle: function () {
-            return this.getOwnerComponent().getModel("i18n").getResourceBundle();
+        _clearDetail: function () {
+            var oDetailPage = this.byId("detailPage");
+            var oDetailContent = this.byId("detailContent");
+            var oNoDataText = this.byId("noDataText");
+
+            oDetailContent.setVisible(false);
+            oNoDataText.setVisible(true);
+
+            oDetailPage.unbindElement();
+            this.byId("SplitApp").toDetail(oDetailPage);
+
+            // Clear the items list filter
+            var oItemsList = this.byId("itemsListDetail");
+            var oBinding = oItemsList.getBinding("items");
+            oBinding.filter([]);
+        },
+
+        onOpenCreateDialog: function () {
+            this._items = []; // clear the items array for new input
+            this.getView().getModel("itemsModel").setProperty("/items", this._items); // clear the items model
+
+            if (!this._createDialog) {
+                this._createDialog = this.byId("createMovementDialog");
+            }
+            this._createDialog.open();
+        },
+
+        onCloseCreateDialog: function () {
+            if (this._createDialog) {
+                this._createDialog.close();
+            }
+        },
+
+        _generateRandomId: function () {
+            var chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+            var id = '';
+            for (var i = 0; i < 4; i++) {
+                id += chars.charAt(Math.floor(Math.random() * chars.length));
+            }
+            return id;
+        },
+
+        // New function to generate random numerical ID for items
+        _generateItemRandomId: function () {
+            return Math.floor(100000 + Math.random() * 900000).toString();
+        },
+
+        onSaveNewMovement: function () {
+            var location = this.byId("locationSelect").getSelectedKey();
+            var type = this.byId("typeSelectDialog").getSelectedKey();
+            var date = this.byId("datePicker").getDateValue();
+            var partner = this.byId("partnerInput").getValue();
+            var id = this._generateRandomId(); // Generate unique ID for the movement
+            var user = sap.ushell.Container.getUser().getId();
+            var mandt = 238;
+            var finished = false;
+
+            // Ensure date is in correct format
+            var oDateFormat = sap.ui.core.format.DateFormat.getDateTimeInstance({ pattern: "yyyy-MM-dd'T'HH:mm:ss" });
+            var formattedDate = oDateFormat.format(date);
+
+            // Get the current date and time
+            var currentDate = new Date();
+            var formattedCurrentDate = oDateFormat.format(currentDate);
+
+            var newMovement = {
+                Id: id,
+                Type: type,
+                MovDate: formattedDate,
+                ChgDate: formattedCurrentDate,
+                ChgUser: user,
+                Partner: partner,
+                Location: location,
+                Finished: finished
+            };
+
+            var oModel = this.getView().getModel();
+            var oView = this.getView();
+
+            // Create the movement
+            oModel.create("/MovementSet", newMovement, {
+                success: function () {
+                    MessageToast.show(" movement created ");
+
+                 
+                    var items = this._items;
+                    items.forEach(function (item, index) {
+                        var newItem = {
+                            MovId: id,
+                            Matnr: item.materialNumber,
+                            Umziz: item.quantity,
+                            Meins: item.unit,
+                            ItemId: this._generateItemRandomId()
+                        };
+                        console.log("Item: ", newItem);
+
+                      
+                        var groupId = "batchRequest" + index;
+                        oModel.create("/ItemSet", newItem, {
+                            groupId: groupId,
+                            success: function () {
+                                console.log("Item created : ", newItem);
+                                // Update the items model to reflect the new item
+                                var oItemsModel = oView.getModel("itemsModel");
+                                if (index === items.length - 1) {
+                                    oView.byId("createMovementDialog").close();
+                                }
+                            },
+                            error: function () {
+                                MessageToast.show("Error creating item: " + item.materialNumber);
+                            }
+                        });
+
+                        // Submit the batch request for each item
+//I might want to batch all item creations together
+                        oModel.submitChanges({
+                            groupId: groupId,
+                            success: function () {
+                                console.log("Batch request successful for groupId: ", groupId);
+                            },
+                            error: function () {
+                                console.log("Batch request failed for groupId: ", groupId);
+                            }
+                        });
+                    }.bind(this));
+
+                }.bind(this),
+                error: function () {
+                    MessageToast.show("Error creating new movement");
+                }
+            });
+
+            console.log("New Movement: ", newMovement);
+        },
+
+
+
+        onOpenAddItemDialog: function () {
+            if (!this._addItemDialog) {
+                this._addItemDialog = this.byId("addItemDialog");
+            }
+            this._addItemDialog.open();
+        },
+
+        onCloseAddItemDialog: function () {
+            if (this._addItemDialog) {
+                this._addItemDialog.close();
+            }
+        },
+
+        onAddItem: function () {
+            var materialNumber = this.byId("materialNumberInput").getValue();
+            var quantity = this.byId("quantityInput").getValue();
+            var unit = this.byId("unitInput").getValue();
+
+            var newItem = { materialNumber, quantity, unit };
+            this._items.push(newItem);
+
+            var oItemsModel = this.getView().getModel("itemsModel");
+            oItemsModel.setProperty("/items", this._items);
+
+            this.onCloseAddItemDialog();
+        },
+
+        onDeleteEntry: function () {
+            if (!this._deleteEntryId) {
+                this._deleteEntryId = null;
+            }
+            var oDetailPage = this.byId("detailPage");
+            this._deleteEntryId = oDetailPage.getBindingContext().getProperty("Id");
+
+            if (!this._confirmDeleteDialog) {
+                this._confirmDeleteDialog = this.byId("confirmDeleteDialog");
+            }
+            this._confirmDeleteDialog.open();
+        },
+
+        onCloseConfirmDeleteDialog: function () {
+            if (this._confirmDeleteDialog) {
+                this._confirmDeleteDialog.close();
+            }
+        },
+
+        onConfirmDelete: function () {
+            var sPath = "/MovementSet('" + this._deleteEntryId + "')";
+            var oModel = this.getView().getModel();
+
+            oModel.remove(sPath, {
+                success: function () {
+                    MessageToast.show("Movement deleted successfully");
+                    this.onCloseConfirmDeleteDialog();
+                }.bind(this),
+                error: function () {
+                    MessageToast.show("Error deleting movement");
+                }
+            });
+
+            console.log("Delete Movement ID: ", this._deleteEntryId);
         }
     });
 });
